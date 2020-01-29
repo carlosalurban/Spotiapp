fd7a004191ee7aa25f61c3931d84e5873961390f
carlosalvarezurbano@gmail.com <carlosalvarezurbano@gmail.com>
2020-01-29 22:41:49 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// ROUTES
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+// HTTP
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
